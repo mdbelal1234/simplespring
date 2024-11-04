5304892e90fcb0e718eab5b082047f7ae673a924
belal.uddin <belal.uddin@ext.azerion.com>
2024-11-05 01:28:23 +0600

    add @RequestMapping annotation

--- src/main/java/org/codemaster/litespringboot/annotations/RequestMapping.java
+++ src/main/java/org/codemaster/litespringboot/annotations/RequestMapping.java
@@ -3,9 +3,12 @@ package org.codemaster.litespringboot.annotations;
 import org.codemaster.litespringboot.enums.MethodType;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
 public @interface RequestMapping {
     String url() default "";
     MethodType type() default MethodType.GET;
